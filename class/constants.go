7ef8c9bb499cc17aca8c49f6cbb429d7420cd5be
jvmakine <jvmakine@gmail.vom>
2023-04-20 21:48:39 +1000

    refactoring

--- class/constants.go
+++ class/constants.go
@@ -1,6 +1,10 @@
 package class
 
-import "github.com/jvmakine/goasm/classfile"
+import (
+	"fmt"
+
+	"github.com/jvmakine/goasm/classfile"
+)
 
 type Constants struct {
 	file *classfile.Classfile
@@ -47,4 +51,48 @@ func (c *Constants) NewClassInfo(name *String) *ClassInfo {
 		Info: nameIndex,
 	})
 	return &ClassInfo{c.file, uint16(len(c.file.ConstantPool))}
+}
+
+type ClassInfo struct {
+	file  *classfile.Classfile
+	index uint16
+}
+
+func (ci *ClassInfo) Name() *String {
+	classinfo := ci.file.ConstantPool[ci.index-1]
+	if classinfo.Tag != classfile.CONSTANT_Class {
+		panic(fmt.Errorf("incorrect class info constant type: %d", classinfo.Tag))
+	}
+	nameIndex := classfile.Order.Uint16(classinfo.Info)
+	return &String{ci.file, nameIndex}
+}
+
+type String struct {
+	file  *classfile.Classfile
+	index uint16
+}
+
+func (s *String) Text() string {
+	entry := s.file.ConstantPool[s.index-1]
+	if entry.Tag != classfile.CONSTANT_Utf8 {
+		panic(fmt.Errorf("incorrect string constant type: %d", entry.Tag))
+	}
+	length := classfile.Order.Uint16(entry.Info)
+	if len(entry.Info) != int(length)+2 {
+		panic(fmt.Errorf("incorrect string length: %d, expected %d", len(entry.Info)-2, length))
+	}
+	return string(entry.Info[2:])
+}
+
+func (s *String) SetText(to string) {
+	entry := s.file.ConstantPool[s.index-1]
+	if entry.Tag != classfile.CONSTANT_Utf8 {
+		panic(fmt.Errorf("incorrect string constant type: %d", entry.Tag))
+	}
+	bytes := []byte(to)
+
+	lengthBytes := make([]byte, 2)
+	classfile.Order.PutUint16(lengthBytes, uint16(len(bytes)))
+
+	entry.Info = append(lengthBytes, bytes...)
 }
